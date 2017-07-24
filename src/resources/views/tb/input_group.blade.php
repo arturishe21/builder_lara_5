30fdd3738f3716a006f5e1c35775d5db33d9967e
Vadim Mozharovskiy <vadimaskan@gmail.com>
2017-07-24 14:25:54 +0300

    sortable group input

--- src/resources/views/tb/input_group.blade.php
+++ src/resources/views/tb/input_group.blade.php
@@ -1,47 +1,56 @@
-
 <div class="group" name="{{$name}}">
     <div class="other_section">
-        @foreach($rows as $k => $filds)
-            <div class="section_group">
-                <a class="delete_group" onclick="TableBuilder.deleteGroup(this)" style="float: right; cursor: pointer; position: absolute; right: 0; z-index: 10"><i class="fa red fa-times"></i> Удалить</a>
-                @foreach($filds as $titleField => $fild)
-                    <section class="{{$fild['class_name'] or ''}}" @if(isset($fild['tabs'])) style="margin-top:20px" @endif>
 
-                        @if (!isset($fild['tabs']))
-                            <label class="label">{{$fild['caption']}}</label>
-                        @endif
-                        <div style="position: relative;">
-                            <div class="tabs_section">
-                                {!! $fild['html'] !!}
-                                @if (isset($fild['multi']) && $fild['multi'])
-                                    <input type="hidden" name="{{$titleField}}" value=''>
+        <ul id="sortable">
+            @foreach($rows as $k => $filds)
+                <li>
+                    <div class="section_group">
+                        <a class="delete_group" onclick="TableBuilder.deleteGroup(this)"
+                           style="float: right; cursor: pointer; position: absolute; right: 0; z-index: 10"><i
+                                    class="fa red fa-times"></i> Удалить</a>
+                        @foreach($filds as $titleField => $fild)
+                            <section class="{{$fild['class_name'] or ''}}"
+                                     @if(isset($fild['tabs'])) style="margin-top:20px" @endif>
+
+                                @if (!isset($fild['tabs']))
+                                    <label class="label">{{$fild['caption']}}</label>
                                 @endif
-                            </div>
-                        </div>
-                    </section>
-                @endforeach
-            </div>
-        @endforeach
+                                <div style="position: relative;">
+                                    <div class="tabs_section">
+                                        {!! $fild['html'] !!}
+                                        @if (isset($fild['multi']) && $fild['multi'])
+                                            <input type="hidden" name="{{$titleField}}" value=''>
+                                        @endif
+                                    </div>
+                                </div>
+                            </section>
+                        @endforeach
+                    </div>
+                </li>
+            @endforeach
+
+        </ul>
 
     </div>
     @if (!$hide_add)
-        <a class="add_group" onclick="TableBuilder.addGroup(this); groupTabsRefresh('{{$name}}');"><i class="fa fa-plus-square"></i> Добавить</a>
+        <a class="add_group" onclick="TableBuilder.addGroup(this); groupTabsRefresh('{{$name}}');"><i
+                    class="fa fa-plus-square"></i> Добавить</a>
     @endif
 </div>
 <script>
 
     var i = 0;
-    $(".group[name={{$name}}] input, .group[name={{$name}}] select, .group[name={{$name}}] textarea" ).each(function( index ) {
+    $(".group[name={{$name}}] input, .group[name={{$name}}] select, .group[name={{$name}}] textarea").each(function (index) {
 
         if ($(this).attr("name") != undefined) {
             i++;
             $(this).attr("id", "{{$name}}_" + $(this).attr("name"));
-            $(this).attr("name", "{{$name}}[" + $(this).attr("name")+ "][]");
+            $(this).attr("name", "{{$name}}[" + $(this).attr("name") + "][]");
 
             if ($(this).attr("data-multi") == 'multi') {
                 $(this).removeAttr('name');
             }
-          //  $(this).addClass($(this).attr("id") + '_' + i);
+            //  $(this).addClass($(this).attr("id") + '_' + i);
         }
     });
 
@@ -49,17 +58,32 @@
     function groupTabsRefresh(name) {
 
         i = 0;
-        $(".group[name=" + name + "] .tabs_section").each(function(){
+        $(".group[name=" + name + "] .tabs_section").each(function () {
             i++;
-            $(this).find(".nav-tabs a").each(function(){
+            $(this).find(".nav-tabs a").each(function () {
                 var hrefOld = $(this).attr("href");
                 $(this).attr("href", hrefOld + "_" + i);
             });
-            $(this).find(".tab-content .tab-pane").each(function(){
+            $(this).find(".tab-content .tab-pane").each(function () {
                 var idOld = $(this).attr("id");
                 $(this).attr("id", idOld + "_" + i);
             });
         });
     }
     groupTabsRefresh('{{$name}}');
+</script>
+
+<style>
+    #sortable {
+        list-style: none;
+        margin: 0px;
+        padding: 0px;
+        cursor: move;
+    }
+</style>
+<script>
+    $(function () {
+        $("#sortable").sortable();
+        $("#sortable").disableSelection();
+    });
 </script>
