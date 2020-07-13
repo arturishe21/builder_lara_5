8b4c96f805872a3d981ecef7cc933da6a1cd40c9
Artur <artur@vis-design.com>
2020-07-13 16:29:16 +0300

    add generate password

--- src/Http/Fields/Password.php
+++ src/Http/Fields/Password.php
@@ -2,10 +2,19 @@
 
 namespace Vis\Builder\Fields;
 
+use Illuminate\Support\Facades\Hash;
+
 class Password extends Field
 {
     public function getValueForList($definition)
     {
         return '*****';
+    }
+
+    public function prepareSave($request)
+    {
+        $nameField = $this->getNameField();
+
+        return Hash::make($request[$nameField]);
     }
 }
