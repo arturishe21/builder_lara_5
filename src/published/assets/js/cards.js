d87d8f17144b9125d12d992c91caedad144a4ab2
artur <artur@vis-desing.com>
2021-11-08 16:46:49 +0200

    add new chart

--- src/published/assets/js/cards.js
+++ src/published/assets/js/cards.js
@@ -1,11 +1,9 @@
-"use strict";
-
 var Cards =
     {
+        myLineChart : '',
+
         init: function()
         {
-            Cards.rangeChange();
-
             $(".datepicker_trend").datepicker({
                 changeMonth: true,
                 numberOfMonths: 1,
@@ -19,85 +17,61 @@ var Cards =
 
             $('.datepicker_trend').change(function () {
                 var contentTrend = $(this).parents('article');
-                Cards.loadChart(contentTrend);
+                Cards.updateData(contentTrend);
             });
-
 
             $('.trends').each(function( index ) {
                 var contentTrend = $(this).parents('article');
-                Cards.loadChart(contentTrend);
+
+                Cards.updateData(contentTrend);
             });
-        },
 
-        rangeChange : function () {
-            $('[name=range]').change(function () {
+            if ($( ".trends" ).size()) {
 
-                var data = {
-                    'model': $(this).attr('data-model'),
-                    'range' :  $(this).val(),
-                };
+                var ctx = document.querySelector('canvas').getContext('2d');
 
-                var cardId = $('#' + $(this).attr('data-card-id'));
-
-                var $posting = jQuery.post('/admin/change-range-card', data);
-
-                $posting.done(function (response) {
-                    cardId.find('.value_current').html(response.current);
-                    cardId.find('.value_difference').html(response.difference);
-                });
-            });
-        },
-
-        loadChart : function (contentTrend)
-        {
-            var data = {
-                'from' : contentTrend.find('[name=trend_from]').val(),
-                'to' : contentTrend.find('[name=trend_to]').val(),
-                'model' : contentTrend.find('[name=trend_model]').val(),
-            }
-
-            var $posting = jQuery.post('/admin/change-range-trend', data);
-
-            $posting.done(function (response) {
-
-                var data = {
-                    "xScale": "time",
-                    "yScale": "linear",
-                    "main": [{
-                        className: ".stats",
-                        "data": response
-                    }]
-                };
-
-                var opts = {
-                    paddingLeft: 25,
-                    paddingTop: 10,
-                    paddingRight: 15,
-                    axisPaddingLeft: 25,
-                    //tickHintX: 4, // How many ticks to show horizontally
-                    tickHintY: 6,
-                    dataFormatX: function (x) {
-                        return Date.create(x);
+                Cards.myLineChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: [],
+                        datasets: [{
+                            label: 'Count',
+                            data: [],
+                            backgroundColor: 'rgb(255, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                        }]
                     },
-
-                    tickFormatX: function (x) {
-                        return x.format('{dd}/{MM}');
-                    },
-
-                    "mouseover": function (d, i) {
-
-                    },
-
-                    "mouseout": function (x) {
-
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        plugins: {
+                            legend: {
+                                display: false,
+                            }
+                        }
                     }
-                };
+                });
+            }
+        },
+        updateData : function (contentTrend) {
+            $.ajax({
+                url: '/admin/change-range-trend',
+                method: 'POST',
+                data: {
+                    'from' : contentTrend.find('[name=trend_from]').val(),
+                    'to' : contentTrend.find('[name=trend_to]').val(),
+                    'model' : contentTrend.find('[name=trend_model]').val()
+                },
+                dataType: 'json',
+                success: function (d) {
 
-                var chartTrend = contentTrend.find('.trends').attr('id');
+                    // assign programmatically the datasets again, otherwise data changes won't show
+                    Cards.myLineChart.data.labels = d.labels;
+                    Cards.myLineChart.data.datasets[0].data = d.values;
 
-                new xChart('line-dotted', data, '#' + chartTrend, opts);
+                    Cards.myLineChart.update();
+                }
             });
         }
-
-};
+    };
 Cards.init();
