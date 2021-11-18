567a2f427202995a8aff3ec8b333a73956cdcd39
artur <artur@vis-desing.com>
2021-11-18 14:55:23 +0200

    fix show in list and exel

--- src/Http/Fields/MultiFile.php
+++ src/Http/Fields/MultiFile.php
@@ -4,10 +4,40 @@ namespace Vis\Builder\Fields;
 
 class MultiFile extends File
 {
-    public $onlyForm = true;
-
     public function getValueArray()
     {
         return json_decode($this->getValue());
     }
+
+    public function getValueForList($definition)
+    {
+        $collections = json_decode($this->getValue());
+        $result = [];
+
+        if ($this->getValue() && count($collections)) {
+
+            $result = array_map(function ($file) {
+                return "<a href='{$file}'>". basename($file). "<a>";
+            }, $collections);
+        }
+
+        return implode('<br>', $result);
+    }
+
+    public function getValueForExel($definition)
+    {
+        $collections = json_decode($this->getValue());
+        $result = [];
+
+        if ($this->getValue() && count($collections)) {
+
+            $result = array_map(function ($file) {
+                return request()->getSchemeAndHttpHost() . $file;
+            }, $collections);
+        }
+
+        return implode(', ', $result);
+    }
 }
+
+
