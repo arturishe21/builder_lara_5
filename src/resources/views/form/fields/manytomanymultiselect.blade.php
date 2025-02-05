51f37746c7954408c1c7c8f636c10ca627dcb714
Artur Shchablevskyi <artursh@xmlshop.com>
2025-02-05 11:49:01 +0200

    added label for field

--- src/resources/views/form/fields/manytomanymultiselect.blade.php
+++ src/resources/views/form/fields/manytomanymultiselect.blade.php
@@ -6,23 +6,22 @@ $selected = $field->getOptionsSelected($definition);
 	.ui-multiselect .ui-widget-header,.ui-multiselect  .ui-widget-header  a {
 		color: #fff;
 	}
-	.ui-multiselect {
-		margin-bottom: 10px;
-	}
 </style>
-
-<select class="multiselect"  multiple="multiple" name="{{ $field->getNameField()}}[]" id="{{ $field->getNameField()}}">
-	@if (isset($selected) && count($selected))
-		@foreach($selected as $id => $selectOption)
-			<option value="{{$id}}" selected>{{$selectOption}}</option>
-		@endforeach
-	@endif
-	@foreach ($field->getOptions($definition) as $key => $title)
-		@if (!isset($selected[$key]))
-			<option value="{{$key}}">{{ trim($title) }}</option>
+<section class="{{$field->getClassName()}}">
+	<label class="label" for="{{ $field->getNameField()}}">{{$field->getName()}}</label>
+	<select class="multiselect"  multiple="multiple" name="{{ $field->getNameField()}}[]" id="{{ $field->getNameField()}}">
+		@if (isset($selected) && count($selected))
+			@foreach($selected as $id => $selectOption)
+				<option value="{{$id}}" selected>{{$selectOption}}</option>
+			@endforeach
 		@endif
-	@endforeach
-</select>
+		@foreach ($field->getOptions($definition) as $key => $title)
+			@if (!isset($selected[$key]))
+				<option value="{{$key}}">{{ trim($title) }}</option>
+			@endif
+		@endforeach
+	</select>
+</section>
 
 <script type="text/javascript">
 	$(document).ready(function () {
